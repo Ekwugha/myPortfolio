9caea55b33633d75db761cdb139eaad02132d09c
Ekwugha <elloekwugha@gmail.com>
2026-01-13 13:54:53 +0100

    Refactor favicon and app icon links in Document component to use SVG format for modern browsers

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,21 +10,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Favicon and app icons */}
+        {/* Favicon and app icons - SVG for modern browsers */}
         <link rel="icon" type="image/svg+xml" href="/logo.svg" />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="apple-touch-icon" sizes="180x180" href="/logo-256.png" />
+        <link rel="shortcut icon" href="/logo.svg" />
+        <link rel="apple-touch-icon" href="/logo.svg" />
         <meta name="theme-color" content="#020617" />
 
         {/* Primary Meta Tags */}
